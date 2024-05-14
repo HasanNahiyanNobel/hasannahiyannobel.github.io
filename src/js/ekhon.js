46c22171de2e76a52e0a6a4d3e8bb578d8f3e68b
HasanNahiyanNobel <hasan.nahiyan.nobel@gmail.com>
2024-05-14 19:22:44 +0600

    Renamed elements and soft-coded some parts for better readability.

--- src/js/ekhon.js
+++ src/js/ekhon.js
@@ -4,16 +4,14 @@ function startEkhon() {
     // Add the custom CSS file
     document.head.innerHTML += `<link rel="stylesheet" href="css/ekhon.min.css">`
 
-    // Get viewport height and width
-    const vw = Math.max(document.documentElement.clientWidth || 0,
-        window.innerWidth || 0); // Taken from: https://stackoverflow.com/a/8876069
+    // Get viewport height
     const vh = Math.max(document.documentElement.clientHeight || 0,
         window.innerHeight || 0); // Also from: https://stackoverflow.com/a/8876069
 
     // Get the document elements
-    const mainDivBase = document.getElementById(`md`)
+    const mainDivOfBase = document.getElementById(`md`)
     const mainNavbar = document.getElementById(`mn`);
-    const btnOkay = document.getElementById(`ekhon-accha`);
+    const btnModalOkay = document.getElementById(`ekhon-modal-okay`);
     const introVideoDiv = document.getElementById(`ekhon-intro`);
     const introVideo = document.getElementById(`ekhon-intro-vid`);
     const mainDivs = [
@@ -21,15 +19,19 @@ function startEkhon() {
         document.getElementById(`ekhon-md-2`),
         document.getElementById(`ekhon-md-3`),
     ]
-    const midVideo = document.getElementById(`ekhon-mid-vid`);
     const midVideoDiv = mainDivs[1];
-    const entranceLines = Array.from(document.getElementsByClassName(`ekhon-entrance`));
-    const playButton = document.querySelector(`.play-button`);
+    const midVideo = document.getElementById(`ekhon-mid-vid`);
+    const entranceElements = Array.from(document.getElementsByClassName(`ekhon-entrance`));
+    const playButton = document.querySelector(`.play-btn`);
+    const playButtonIconHTML = `<i class="bi bi-play-circle"></i>`;
 
     // Define the constant values
     const transitionTimeInMS = 2000;
     const transitionSpeedCurve = `linear`;
-    const entranceLineAppearanceIntervalInMS = 100; // TODO: Make this 1000
+    const entranceElementAppearanceIntervalInMS = 100; // TODO: Make this 1000
+
+    // Define the state variables
+    let arePaddingsInBaseState = true;
 
     // Calculate the derived values
     const mainNavbarHeight = mainNavbar.offsetHeight;
@@ -39,10 +41,13 @@ function startEkhon() {
     introVideo.style.height = `${vh - mainNavbarHeight - 6}px`; // Somehow there's a 6 pixel gap—don't know why
     introVideo.style.objectFit = `cover`;
 
-    // Make the entrance lines invisible and add transitions
-    entranceLines.forEach(line => {
-        line.style.opacity = `0`;
-        line.style.transition = `opacity ${transitionTimeInMS}ms ${transitionSpeedCurve}`;
+    // Set the play button icon
+    playButton.innerHTML += playButtonIconHTML;
+
+    // Make the entrance elements invisible and add transitions
+    entranceElements.forEach(element => {
+        element.style.opacity = `0`;
+        element.style.transition = `opacity ${transitionTimeInMS}ms ${transitionSpeedCurve}`;
     });
 
     // Trigger the modal which requests reader to use headphones
@@ -50,50 +55,59 @@ function startEkhon() {
         document.getElementById(`ekhon-mt`).click();
     };
 
-    // Remove navbar padding while playing the video
-    mainNavbar.classList.remove(`mb-3`);
-    // Remove main div padding while playing the video
-    mainDivBase.classList.remove(`mb-4`);
+    // Remove navbar and base's main div padding while playing the video
+    switchPaddings();
 
-    // Play the video when user clicked `accha`
-    btnOkay.addEventListener(`click`, function () {
+    // Play the intro video when the reader clicks `আচ্ছা`
+    btnModalOkay.addEventListener(`click`, function () {
         // introVideoDiv.classList.remove(`d-none`);
         // introVideo.play();
         // introVideo.addEventListener(`ended`, postIntroVideoRoutine);
-        postIntroVideoRoutine();
+        postIntroVideoRoutine(); // TODO: Remove this debug call
     })
 
     function postIntroVideoRoutine() {
-        // Make the video invisible
+        // Make the intro video invisible
         introVideoDiv.classList.add(`d-none`);
-        // Add navbar padding while playing the video
-        mainNavbar.classList.add(`mb-3`);
-        // Add main div padding while playing the video
-        mainDivBase.classList.add(`mb-4`);
+        // Bring paddings to the base state
+        switchPaddings();
         // Make the story visible
         mainDivs.forEach(mainDiv => {
             mainDiv.classList.remove(`d-none`);
         });
-        // Fade in the entrance lines
-        fadeInEntranceLines();
-        // Play "Koto Gai" by Shaanta when the user clicks
-        let isListening = true;
+        // Fade in the entrance elements
+        fadeInEntranceElements();
+        // Play "Koto Gai" by Shaanta when the reader clicks
+        let isMidVideoDivListening = true;
         midVideoDiv.addEventListener(`click`, playAndPauseVideo);
         // Stop the option to replay "Koto Gai" by Shaanta once the video ends
         midVideo.addEventListener(`ended`, function () {
-            if (isListening) {
+            if (isMidVideoDivListening) {
                 midVideoDiv.removeEventListener(`click`, playAndPauseVideo);
-                isListening = false;
+                isMidVideoDivListening = false;
             }
         });
     }
 
-    function fadeInEntranceLines() {
-        entranceLines.forEach((line, index) => {
-            let lineWillAppearAfterMS = entranceLineAppearanceIntervalInMS * (index + 1);
+    function switchPaddings() {
+        if (arePaddingsInBaseState) {
+            // Remove the paddings
+            mainNavbar.classList.remove(`mb-3`);
+            mainDivOfBase.classList.remove(`mb-4`);
+        } else {
+            // Add the paddings
+            mainNavbar.classList.add(`mb-3`);
+            mainDivOfBase.classList.add(`mb-4`);
+        }
+        arePaddingsInBaseState = !arePaddingsInBaseState;
+    }
+
+    function fadeInEntranceElements() {
+        entranceElements.forEach((element, index) => {
+            let elementWillAppearAfterMS = entranceElementAppearanceIntervalInMS * (index + 1);
             setTimeout(() => {
-                line.style.opacity = `1`;
-            }, lineWillAppearAfterMS);
+                element.style.opacity = `1`;
+            }, elementWillAppearAfterMS);
         });
     }
 
@@ -103,7 +117,7 @@ function startEkhon() {
             playButton.textContent = ``;
         } else {
             midVideo.pause();
-            playButton.innerHTML += `<i class="bi bi-play-circle"></i>`;
+            playButton.innerHTML += playButtonIconHTML;
         }
     }
 }

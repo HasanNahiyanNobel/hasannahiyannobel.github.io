c77659982916e18364a777898bce88d5e7113efd
HasanNahiyanNobel <hasan.nahiyan.nobel@gmail.com>
2024-05-14 18:17:23 +0600

    Added the video "Koto Gai" by Shaanta.

--- src/js/ekhon.js
+++ src/js/ekhon.js
@@ -1,6 +1,9 @@
 startEkhon()
 
 function startEkhon() {
+    // Add the custom CSS file
+    document.head.innerHTML += `<link rel="stylesheet" href="css/ekhon.min.css">`
+
     // Get viewport height and width
     const vw = Math.max(document.documentElement.clientWidth || 0,
         window.innerWidth || 0); // Taken from: https://stackoverflow.com/a/8876069
@@ -8,18 +11,25 @@ function startEkhon() {
         window.innerHeight || 0); // Also from: https://stackoverflow.com/a/8876069
 
     // Get the document elements
-    const mainDiv = document.getElementById(`md`)
+    const mainDivBase = document.getElementById(`md`)
     const mainNavbar = document.getElementById(`mn`);
     const btnOkay = document.getElementById(`ekhon-accha`);
     const introVideoDiv = document.getElementById(`ekhon-intro`);
     const introVideo = document.getElementById(`ekhon-intro-vid`);
-    const mainDivOfThisStory = document.getElementById(`ekhon-md`);
+    const mainDivs = [
+        document.getElementById(`ekhon-md-1`),
+        document.getElementById(`ekhon-md-2`),
+        document.getElementById(`ekhon-md-3`),
+    ]
+    const midVideo = document.getElementById(`ekhon-mid-vid`);
+    const midVideoDiv = mainDivs[1];
     const entranceLines = Array.from(document.getElementsByClassName(`ekhon-entrance`));
+    const playButton = document.querySelector(`.play-button`);
 
     // Define the constant values
     const transitionTimeInMS = 2000;
     const transitionSpeedCurve = `linear`;
-    const entranceLineAppearanceIntervalInMS = 1000;
+    const entranceLineAppearanceIntervalInMS = 100; // TODO: Make this 1000
 
     // Calculate the derived values
     const mainNavbarHeight = mainNavbar.offsetHeight;
@@ -43,14 +53,14 @@ function startEkhon() {
     // Remove navbar padding while playing the video
     mainNavbar.classList.remove(`mb-3`);
     // Remove main div padding while playing the video
-    mainDiv.classList.remove(`mb-4`);
+    mainDivBase.classList.remove(`mb-4`);
 
     // Play the video when user clicked `accha`
     btnOkay.addEventListener(`click`, function () {
-        introVideoDiv.classList.remove(`d-none`);
-        introVideo.play();
-        introVideo.addEventListener(`ended`, postIntroVideoRoutine);
-        // postIntroVideoRoutine();
+        // introVideoDiv.classList.remove(`d-none`);
+        // introVideo.play();
+        // introVideo.addEventListener(`ended`, postIntroVideoRoutine);
+        postIntroVideoRoutine();
     })
 
     function postIntroVideoRoutine() {
@@ -59,11 +69,23 @@ function startEkhon() {
         // Add navbar padding while playing the video
         mainNavbar.classList.add(`mb-3`);
         // Add main div padding while playing the video
-        mainDiv.classList.add(`mb-4`);
+        mainDivBase.classList.add(`mb-4`);
         // Make the story visible
-        mainDivOfThisStory.classList.remove(`d-none`);
+        mainDivs.forEach(mainDiv => {
+            mainDiv.classList.remove(`d-none`);
+        });
         // Fade in the entrance lines
         fadeInEntranceLines();
+        // Play "Koto Gai" by Shaanta when the user clicks
+        let isListening = true;
+        midVideoDiv.addEventListener(`click`, playAndPauseVideo);
+        // Stop the option to replay "Koto Gai" by Shaanta once the video ends
+        midVideo.addEventListener(`ended`, function () {
+            if (isListening) {
+                midVideoDiv.removeEventListener(`click`, playAndPauseVideo);
+                isListening = false;
+            }
+        });
     }
 
     function fadeInEntranceLines() {
@@ -73,5 +95,15 @@ function startEkhon() {
                 line.style.opacity = `1`;
             }, lineWillAppearAfterMS);
         });
+    }
+
+    function playAndPauseVideo() {
+        if (midVideo.paused) {
+            midVideo.play();
+            playButton.textContent = ``;
+        } else {
+            midVideo.pause();
+            playButton.innerHTML += `<i class="bi bi-play-circle"></i>`;
+        }
     }
 }
